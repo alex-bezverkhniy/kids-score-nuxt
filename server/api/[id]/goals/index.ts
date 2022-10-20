4019b4570e92bc02c74ad54217cdc2d326dc4f06
Alex Bezverkhniy <alexander.bezverkhniy@gmail.com>
2022-10-20 16:16:21 -0500

    goals api

---
+++ server/api/[id]/goals/index.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    const id = event.context.params.id;
+    console.log(`get all goals for ${id}`);
+
+    const data = await useStorage().getItem(`redis:goals:${id}`)
+    event.res.setHeader("Content-Type", "application/json")
+    console.debug("goals", data)
+    return data
+})
