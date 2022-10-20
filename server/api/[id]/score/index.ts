fa3b2293b5eaf80e25ea91f4432c543a6b82db09
Alex Bezverkhniy <alexander.bezverkhniy@gmail.com>
2022-10-20 16:11:48 -0500

    score + reorganized

--- server/api/[id]/score/index.ts
+++ server/api/[id]/score/index.ts
@@ -1,9 +1,9 @@
 export default defineEventHandler(async (event) => {
     const id = event.context.params.id;
-    console.log(`get all tasks for ${id}`);
+    console.log(`get all score for ${id}`);
 
-    const data = await useStorage().getItem(`redis:tasks:${id}`)
+    const data = await useStorage().getItem(`redis:score:${id}`)
     event.res.setHeader("Content-Type", "application/json")
-    console.debug("tasks", data)
+    console.debug("score", data)
     return data
 })
